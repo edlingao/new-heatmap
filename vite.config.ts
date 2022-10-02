cc7642a43b65bafeddf53e816ccf4b4ab20323ae
edlingao <sonido244@gmail.com>
2022-10-02 03:38:43 -0500

    adding extra config to solve JSON files issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), viteStaticCopy({
+    targets: [
+      {
+        src: path.resolve(__dirname, './src/assets/json') + '/[!.]*', // 1️⃣
+        dest: './src/assets/json', // 2️⃣
+      },
+    ],
+  }),],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
   },
-  optimizeDeps: {include: ['mapbox-gl', 'maplibre-gl']}
+  optimizeDeps: {include: ['mapbox-gl', 'maplibre-gl']},
 });
